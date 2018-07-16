429a54eb34b916d8c0d313921c0347531d1ba520
Laura Petre <laura.d.petre@gmail.com>
2018-07-16 13:36:12 +0100

    Added new feature test for printed statement

--- jasmine-standalone-3.1.0/spec/FeatureSpec.js
+++ jasmine-standalone-3.1.0/spec/FeatureSpec.js
@@ -5,4 +5,14 @@ describe('Feature', () => {
     bankAccount.deposit(500);
     expect(bankAccount.balance()).toEqual(500);
   });
+
+  it('a user can see a bank statement', () => {
+    let statement = new BankStatement;
+    let bankAccount = new BankAccount(statement);
+    let printer = new Printer(statement);
+    bankAccount.deposit(100);
+    console.log = jasmine.createSpy("pretty message");
+    let task = printer.viewStatement();
+    expect(console.log).toHaveBeenCalledWith(task);
+  });
 });
