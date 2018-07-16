47700a7d61a3fceeb71800d59eb469dc0634e7ef
Laura Petre <laura.d.petre@gmail.com>
2018-07-16 11:44:34 +0100

    Wrote 2nd unit test and minimum amount of code to satisfy the failing tests - both unit tests and feature tests run green

--- jasmine-standalone-3.1.0/spec/BankAccountSpec.js
+++ jasmine-standalone-3.1.0/spec/BankAccountSpec.js
@@ -3,5 +3,11 @@ describe('Bank Account', () => {
   it('is defined', () => {
     let bankAccount = new BankAccount;
     expect(bankAccount).toBeDefined();
-  })
+  });
+
+  it('can handle a deposit', () => {
+    let bankAccount = new BankAccount;
+    bankAccount.deposit(500);
+    expect(bankAccount.balance()).toEqual(500);
+  });
 });
--- jasmine-standalone-3.1.0/src/BankAccount.js
+++ jasmine-standalone-3.1.0/src/BankAccount.js
@@ -1,2 +1,8 @@
 'use strict'
 function BankAccount() {};
+
+BankAccount.prototype.balance = function() {
+  return 500
+};
+
+BankAccount.prototype.deposit = function() {};
