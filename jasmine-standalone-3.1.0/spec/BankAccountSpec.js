61c75af5ae6a9491a1e853288dce4419fe7fc0a9
Laura Petre <laura.d.petre@gmail.com>
2018-07-17 10:32:43 +0100

    Altered name of tests to use floats

--- jasmine-standalone-3.1.0/spec/BankAccountSpec.js
+++ jasmine-standalone-3.1.0/spec/BankAccountSpec.js
@@ -6,16 +6,16 @@ describe('Bank Account', () => {
     bankAccount = new BankAccount;
   });
 
-  it('has a default balance of 0', () => {
+  it('has a default balance of 0.00', () => {
     expect(bankAccount.balance()).toEqual(0.00);
   });
 
-  it('can handle a deposit of 300', () => {
+  it('can handle a deposit of 300.00', () => {
     bankAccount.deposit(300.00);
     expect(bankAccount.balance()).toEqual(300.00);
   });
 
-  it('can handle a withdrawal of 100', () => {
+  it('can handle a withdrawal of 100.00', () => {
     bankAccount.deposit(300.00);
     bankAccount.withdrawal(100.00);
     expect(bankAccount.balance()).toEqual(200.00);
