f37d7cdca63ddc3aaa24e2f95c0ff1ebe91a4aed
Laura Petre <laura.d.petre@gmail.com>
2018-07-18 13:35:03 +0100

    Added test to transaction history

--- jasmine-standalone-3.1.0/spec/TransactionHistorySpec.js
+++ jasmine-standalone-3.1.0/spec/TransactionHistorySpec.js
@@ -1,7 +1,6 @@
 'use strict'
 describe('TransactionHistory', () => {
   let transactionHistory;
-  let expected;
 
   beforeEach(() => {
     transactionHistory = new TransactionHistory;
@@ -9,5 +8,13 @@ describe('TransactionHistory', () => {
 
   it('has a default empty transaction list', () => {
     expect(transactionHistory.showList()).toEqual([]);
+  });
+
+  it('can add a transaction to the list', () => {
+    let amount = 100.00;
+    let balance = 100.00;
+    let date = "18/07/2018";
+    transactionHistory.add(amount, balance);
+    expect(transactionHistory.showList()).toEqual([{"date" : date, "amount" : amount, "balance" : balance}])
   });
 });
